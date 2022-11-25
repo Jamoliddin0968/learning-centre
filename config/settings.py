add84a909888a59d0d679aacdce1841343be708f
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2022-11-25 11:17:37 +0500

    b

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -49,6 +47,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     'rest_framework_simplejwt',
+    'dj_database_url',
 
 ]
 REST_FRAMEWORK = {
