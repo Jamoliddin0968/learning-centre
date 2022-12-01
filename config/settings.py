38dbca3e0426428afd57a1528c6353d4fa4ce2e3
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2022-12-01 12:40:20 +0500

    finish

--- config/settings.py
+++ config/settings.py
@@ -162,8 +162,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 STATIC_URL = 'static/' 
+STATIC_FILESDIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
